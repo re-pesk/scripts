9344bd59b03e947d4ea3b18d978fc1280b23a507
Rėdas Peškaitis <redas.peskaitis@gmail.com>
2024-12-19 06:58:04 +0200

    Pridėti pandoc naudojimo skriptą

---
+++ pandoc/pandoc-bun.mjs
@@ -0,0 +1,97 @@
+#! /usr/bin/env -S bun run
+
+import shell from "shelljs"
+import { parseArgs } from "util";
+
+const standalone = "--standalone"
+const prefix = "lua-"
+const filterfile = "" //"remove-attr.lua" // filtras
+const options = {
+  asciidoc: { ext: "adoc", },
+  context: { ext: "ctex", },
+  djot: { ext: "dj", },
+  docx: { ext: "docx", },
+  haddock: { ext: "hie", },
+  html: { ext: "html", },
+  ipynb: { ext: "ipynb", },
+  jats: { ext: "jats", },
+  json: { ext: "json", },
+  latex: { ext: "tex", },
+  markdown: { ext: "md", opt: "+abbreviations+emoji+mark", },
+  muse: { ext: "muse", },
+  native: { ext: "hs", },
+  pdf: { ext: "pdf", },
+  odt: { ext: "odt", },
+  rst: { ext: "rst", },
+  texinfo: { ext: "txi", },
+  textile: { ext: "textile", },
+  typst: { ext: "typ", },
+}
+
+const { values, positionals } = parseArgs({
+  args: Bun.argv,
+  options: {
+    opt: {
+      type: 'string',
+    },
+  },
+  strict: true,
+  allowPositionals: true,
+})
+
+// tikrina, ar iškvietimo eilutė turi reikalingą argumentą - failo pavadinimą be plėtinio 
+if (positionals.length < 3) {
+  console.log("\nError! There is no filename! Script execution was terminated!\n")
+  shell.exit(99)
+}
+
+// paima iškvietimo eilutės argumentą
+const filename = positionals[2]
+
+// tikrina, ar iškvietimo eilutė turi reikalingą argumentą - tikslinį formatą 
+if (values.opt == undefined) {
+  console.log("\nError! There is no options! Script execution was terminated!\n")
+  shell.exit(99)
+}
+
+const [fromArg, toArg] = values.opt.split("/")
+
+// tikrina, ar egzistuoja pradinio formato duomenys  
+if (!Object.hasOwn(options, fromArg)) {
+  console.log(`\nError! There is no data for ${from}! Script execution was terminated!\n`)
+  shell.exit(99)
+}
+
+// tikrina, ar egzistuoja tikslinio formato duomenys  
+if (!Object.hasOwn(options, toArg)) {
+  console.log(`\nError! There is no data for ${to}! Script execution was terminated!\n`)
+  shell.exit(99)
+}
+
+const fromExt = options[fromArg].ext
+const fromOpt = options[fromArg]?.opt ?? ""
+const toExt = options[toArg].ext
+const toOpt = options[toArg]?.opt ?? ""
+
+// paima dabartinį darbinį katalogą
+const CURDIR = process.cwd()
+
+const filter = `${filterfile ? `--${prefix}filter=${CURDIR}/${filterfile}` : ''}`
+const fileFrom = `"${CURDIR}/${filename}.${fromExt}"`
+const fileTo = `--output="${CURDIR}/${filename}.${toExt}"`
+const formatFrom = `--from=${fromArg}${fromOpt}`
+const formatTo = `--to=${toArg}${toOpt}`
+
+// Sukuria komandos tekstinę eilutę
+const command = `pandoc ${fileFrom} ${standalone} ${filter} ${formatFrom} ${formatTo} ${fileTo}`
+
+// spausdina komandos eilutę
+console.log(command.replaceAll(/[ ]+/g, '\n\t'))
+
+// vykdo komandą, komandos vykdymo rezultatą išsaugo į kintamąjį 
+const code = shell.exec(command).code
+
+// jeigu komanda įvykdyta nesėkmingai, spausdina išėjimo kodą
+if (code !== 0) {
+  console.log(code)
+}
---
+++ pandoc/readme.md
@@ -0,0 +1,41 @@
+# Pandoc koverterio naudojimas
+
+## Paleidimas
+
+Pavyzdys:
+
+```bash
+pandoc-bun.mjs --opt=markdown/asciidoc readme
+```
+
+Parametrai
+
+```bash
+pandoc-bun.mjs \
+--opt=<įvedimo formatas>/<išvedimo formatas> \
+<failo pavadinimas be plėtinio>
+```
+
+Įvedimo išvedimo formatai
+
+| Pavadinimas | Failo plėtinys | Numatytieji plėtiniai |
+| -- | -- | :--: |
+| asciidoc | adoc | - |
+| context | ctex  | - |
+| djot | dj  | - |
+| docx | docx  | - |
+| haddock | hie  | - |
+| html | html  | - |
+| ipynb | ipynb  | - |
+| jats | jats  | - |
+| json | json  | - |
+| latex | tex  | - |
+| markdown | md | +abbreviations+emoji+mark |
+| muse | muse  | - |
+| native | hs  | - |
+| pdf | pdf  | - |
+| odt | odt  | - |
+| rst | rst  | - |
+| texinfo | txi  | - |
+| textile | textile  | - |
+| typst | typ  | - |
