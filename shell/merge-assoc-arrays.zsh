4657c2d7cd71efbba91b70151a32d735b9b26beb
Rėdas Peškaitis <redas.peskaitis@gmail.com>
2025-04-10 20:13:27 +0300

    Atnaujinti darbo su masyvais pavyzdžiai

--- shell/merge-assoc-arrays.zsh
+++ shell/merge-assoc-arrays.zsh
@@ -1,15 +1,28 @@
 #!/usr/bin/env zsh
 
+echo
+
 declare -Ar array1=( [5]=true [10]=true ["15 and ab"]="true and false" )
-declare -Ar array2=( [20]=true [25]=true [30]=true )
+declare -Ar array2=( [20]=true [25]=true [30]=false )
+
+echo 'Show definition of arrays:'
+declare -p array1
+declare -p array2
+
+echo "Size of array1 is ${#array1[@]}"
+echo "Size of array2 is ${#array2[@]}"
+echo
 
 # convert both associative arrays to new associative array
-declare -A array_both=(${(kv)array1} ${(kv)array2})
+declare -A merged_array=(${(kv)array1} ${(kv)array2})
 
-# show array definition
-declare -p array_both
+echo 'Definition of merged_array:'
+declare -p merged_array
+echo "Size of merged_array is ${#merged_array[@]}"
+echo
 
-# iterate on array
-for key val in "${(@kv)array_both}"; do
-  echo "array_both[${key}]=$val"
+echo 'Iterate on merged_array:'
+for key val in "${(@kv)merged_array}"; do
+  echo "merged_array[\"${key}\"] = \"${val}\""
 done
+echo
--- shell/merge-assoc-arrays2.bash
+++ shell/merge-assoc-arrays2.bash
@@ -3,18 +3,32 @@
 # Proposed solution https://stackoverflow.com/a/79352629/29179535 in the thread on the Stack Overflow
 # https://stackoverflow.com/questions/29804909/how-to-combine-associative-arrays-in-bash/79352629#79352629
 
+echo
+
 declare -Ar array1=( [5]=true [10]=true [15]="true and false" )
-declare -Ar array2=( [20]=true [25]=true [30]=true )
+declare -Ar array2=( [20]=true [25]=true [30]=false )
+
+echo 'Definition of arrays:'
+declare -p array1
+declare -p array2
+
+echo "Size of array1 = ${#array1[@]}"
+echo "Size of array2 = ${#array2[@]}"
+echo
 
 # convert both associative arrays to new associative array
-declare -A array_both="($(
+declare -A merged_array="($(
   printf '[%s]="%s" ' "${array1[@]@k}" "${array2[@]@k}"
 ))"
 
-# show array definition
-declare -p array_both
+echo 'Definition of merged_array:'
+declare -p merged_array
 
-# iterate on array
-for key in "${!array_both[@]}"; do
-    echo "array_both[${key}]=${array_both[${key}]}"
+echo "Size of merged_array = ${#merged_array[@]}"
+echo
+
+echo 'Iterate on merged_array:'
+for key in "${!merged_array[@]}"; do
+    echo "merged_array[\"${key}\"] = \"${merged_array[${key}]}\""
 done
+echo
