ce3adc4c21247d8a8828c77364b027f67b571dab
Rėdas Peškaitis <redas.peskaitis@gmail.com>
2025-05-23 20:49:42 +0300

    Nemo įskiepių pataisa

--- shell/nemo-scripts/.local/share/nemo/actions/action_scripts/make-file-executable.bash
+++ shell/nemo-scripts/.local/share/nemo/actions/action_scripts/make-file-executable.bash
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
 filepath="$@"
-echo filepath $filepath
-stat -c %a $filepath
+stat -c %a "$filepath"
 
 if [[ -x "$filepath" ]]; then
   chmod -x "$filepath"
@@ -10,7 +9,7 @@ else
   chmod +x "$filepath"
 fi
 
-stat -c %a $filepath
+stat -c %a "$filepath"
 
 exit 0
 
