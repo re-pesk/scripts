d8c7c80260211c4d24e09a4ebd34be15147074c1
Rėdas Peškaitis <redas.peskaitis@gmail.com>
2026-01-11 13:03:08 +0200

    Atnaujintos Scala diegimo instrukcijos

--- install/scala_install.sh
+++ install/scala_install.sh
@@ -1,23 +1,34 @@
 #!/usr/bin/env bash
 
-[[ $(apt list --installed jq 2> /dev/null | wc -l) == 1 ]] && sudo apt-get install jq 
+. ./helpers.sh
 
-URL="$(curl -Ls -o /dev/null -w %{url_effective} https://github.com/scala/scala3/releases/latest)"
-VERSION="$(basename -- ${URL})"
-curl -sSLo- "${URL//tag/download}/scala3-${VERSION}-x86_64-pc-linux.tar.gz" \
-| tar --transform 'flags=r;s/^(scala3)[^\/]+/\1/x' --show-transformed-names -xzvC "${HOME}/.opt"
+CURRENT_VERSION="$(scala version 2> /dev/null | tail -n 1 | sed 's/.*: //')"
+VERSION="$(basename -- "$(curl -Ls -o /dev/null -w %{url_effective} https://github.com/scala/scala3/releases/latest)")"
+ask_to_install "${CURRENT_VERSION}" "${VERSION}" "Scala" "${HOME}/.opt/scala3"
 
-sed -i '/#begin scala init/,/#end scala init/c\' "${HOME}/.pathrc"
-[[ "$( tail -n 1 "${HOME}/.pathrc" )" =~ ^[[:blank:]]*$ ]] || echo "" >> "${HOME}/.pathrc"
+TMP_DIR="$(mktemp -d)"
+FILE_NAME="scala3-${VERSION}-x86_64-pc-linux.tar.gz"
+URL="https://github.com/scala/scala3/releases/download/${VERSION}/${FILE_NAME}"
+curl -sSLo "${TMP_DIR}/${FILE_NAME}" "${URL}"
+curl -sSLo "${TMP_DIR}/${FILE_NAME}.sha256" "${URL}.sha256"
+compare_sha256 "${TMP_DIR}/${FILE_NAME}" "${TMP_DIR}/${FILE_NAME}.sha256"
 
-echo '#begin scala init
+rm -rf "${HOME}/.opt/scala3"
+tar --file="${TMP_DIR}/${FILE_NAME}" --transform='flags=r;s/^(scala3)[^\/]+/\1/x' --show-transformed-names -xzvC "${HOME}/.opt"
+rm -rf "${TMP_DIR}"
 
-[[ ":${PATH}:" == *":${HOME}/.opt/scala3/bin:"* ]] \
-|| export PATH="${HOME}/.opt/scala3/bin${PATH:+:${PATH}}"
+insert_path '${HOME}/.opt/scala3/bin' 'scala' "${HOME}/.pathrc"
 
-#end scala init' >> "${HOME}/.pathrc"
+[[ ":${PATH}:" == *":${HOME}/.opt/scala3/bin:"* ]] || \
+  export PATH="${HOME}/.opt/scala3/bin${PATH:+:${PATH}}"
 
-[[ ":${PATH}:" == *":${HOME}/.opt/scala3/bin:"* ]] \
-|| export PATH="${HOME}/.opt/scala3/bin${PATH:+:${PATH}}"
+CURRENT_VERSION="$(scala version 2> /dev/null | tail -n 1 | sed 's/.*: //')"
+[[ "${CURRENT_VERSION}" == "${VERSION}" ]] || { 
+  echo "Scala were not updated!"
+  exit 1
+}
+printf "\nScala v${VERSION} is succesfully installed.\n\n"
+printf 'To use without relogging, execute the following command in the terminal:
 
-scala -version
+[[ ":${PATH}:" == *":${HOME}/.opt/scala3/bin:"* ]] || \
+  export PATH="${HOME}/.opt/scala3/bin${PATH:+:${PATH}}"\n\n'
