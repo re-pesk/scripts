28055a58135ae1d028f1e4264e5ec67e9aa7cd0d
Rėdas Peškaitis <redas.peskaitis@gmail.com>
2026-02-26 04:23:04 +0200

    Pašalinti kodo pavyzdžiai, atkartojantys _helpers.sh failą

--- shell/read/read_and_print_enter.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env bash
-
-printf "Do you want overwrite it?
-Print 'y' to overwrite. Print 'n' or <Enter> to exit: \e[s"
-read TO_INSTALL
-[[ "${TO_INSTALL}" == "" ]] && printf "\e[u<Enter>\n"
-printf "\n"
-
-echo "TO_INSTALL: '${TO_INSTALL}'"
