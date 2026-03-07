d2e3dd47c1b37d86ca75b5f5350f2cc56d449891
Rėdas Peškaitis <redas.peskaitis@gmail.com>
2026-03-07 22:30:54 +0200

    Atnaujinti vivaldi diegimo skriptą

--- ppa/vivaldi.sh
+++ ppa/vivaldi.sh
@@ -1,13 +1,34 @@
 #! /usr/bin/env -S bash
 
+# shellcheck disable=SC2034
+APP_NAME="Vivaldi"
+
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+HELPERS="$(cd -- "${SCRIPT_DIR}/../shell/install_helpers/" &> /dev/null && pwd )/_helpers.sh"
+cmp -s "${SCRIPT_DIR}/_helpers.sh" "${HELPERS}" || cp -sfit "${SCRIPT_DIR}/" "${HELPERS}"
+
+# Įkelti pagalbines funkcijas
+. ./_helpers.sh
+
+echo ""
+
+if ! install_missing_packages gpg wget; then
+  exit 1
+fi
+
 # Pagal https://askubuntu.com/questions/1532966/vivaldi-wont-load-on-xubuntu-noble-24-04
 
 wget -qO- https://repo.vivaldi.com/archive/linux_signing_key.pub \
-| sudo gpg --dearmor -o /etc/apt/keyrings/vivaldi-browser.gpg
+| sudo gpg --dearmor -o /usr/share/keyrings/vivaldi-stable.gpg
 
-echo "deb [signed-by=/etc/apt/keyrings/vivaldi-browser.gpg arch=amd64] https://repo.vivaldi.com/archive/deb/ stable main" \
-| sudo tee /etc/apt/sources.list.d/vivaldi-browser.list
+sudo tee /etc/apt/sources.list.d/vivaldi.sources <<SOURCES
+Types: deb
+Architectures: amd64
+URIs: https://repo.vivaldi.com/stable/deb/
+Suites: stable
+Components: main
+Signed-By: /usr/share/keyrings/vivaldi-stable.gpg
+SOURCES
 
 sudo apt update
-
 sudo apt install vivaldi-stable
